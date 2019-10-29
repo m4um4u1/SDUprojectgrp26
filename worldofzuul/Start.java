31772f59befd9af22df60cf987aea358469a4f33
Jessen13 <36962845+Jessen13@users.noreply.github.com>
2019-10-29 15:07:16 +0100

    Bug fixing
    Bug fixing

--- worldofzuul/Start.java
+++ worldofzuul/Start.java
@@ -1,15 +1,10 @@
 package worldofzuul;
 
-import Trash.*;
-
 public class Start {
 
     public static void main(String[] args) {
-        TrashPlastic plast = new TrashPlastic(1,"plast", "det noget random plast");
-        /*
         Game game = new Game();
         game.play();
 
-         */
     }
 }
