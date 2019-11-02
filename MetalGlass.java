52f3af6727d52f08fb8cff760391e90f1ca37c10
Gamm0 <55430079+Gamm0@users.noreply.github.com>
2019-11-02 14:54:12 +0100

    Add files via upload
    Har fået lavet id og trash type i trashbin

---
+++ MetalGlass.java
@@ -0,0 +1,17 @@
+package TrashBin;
+
+
+public class MetalGlass extends TrashBin {
+    private final int trashtype = 3;
+    
+    public MetalGlass(int id, String Place) {
+        super(id,Place);
+    }
+    
+    @Override
+    public void setPlace(String place) {
+        super.setPlace(place);
+        
+    }
+    
+}
---
+++ ResidualWaste.java
@@ -0,0 +1,17 @@
+package TrashBin;
+
+
+public class ResidualWaste extends TrashBin {
+    private final int trashtype = 4;
+
+    public ResidualWaste(int id, String Place) {
+        super(id,Place);
+    }
+
+    @Override
+    public void setPlace(String Place) {
+        super.setPlace(Place); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
