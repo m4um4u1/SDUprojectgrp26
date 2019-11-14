fc93b1464158bf849133981b27356c0d11b73282
m4um4u1 <m4um4u1@gmail.com>
2019-11-14 12:02:30 +0100

    Add files via upload
    TrashBin to Trashbin

---
+++ Trashbin/MetalGlass.java
@@ -0,0 +1,26 @@
+package Trashbin;
+
+import worldofzuul.Room;
+
+public class MetalGlass extends Trashbin {
+    private final int trashtype = 3;
+
+    public MetalGlass(Room place, int id, String trash) {
+        super(place, id, trash);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
+
+    @Override
+    public void setPlace(Room place) {
+        super.setPlace(place);
+    }
+
+    @Override
+    public int getTrashtype() {
+        return trashtype;
+    }
+}
